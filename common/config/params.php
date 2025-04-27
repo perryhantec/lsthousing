d57825740249d65828bb7056c4b4fc5228b4f279
Hong <hong.wong@efaith.com.hk>
2025-04-28 01:18:42 +0800

    ignore params.php

--- common/config/params.php
+++ common/config/params.php
@@ -9,4 +9,8 @@ return [
     'user.passwordResetTokenExpire' => 3600,
     // 'user.passwordMinLength' => 8,
     'icon-framework' => 'fa',
+
+    // 'twilioSid' => 'AC3a735d7c9060e30aeb862ae99b408dd9',
+    // 'twiliotoken' => '41920774c53c332b9a9eafcbbb162dbf',
+    // 'twilioNumber' => '+85262027744',
 ];
