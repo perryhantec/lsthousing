87856d3323d21b622bfd654b6848c34e1df51672
Hong <hong.wong@efaith.com.hk>
2025-04-28 00:42:14 +0800

    comment twilio data

--- common/config/params.php
+++ common/config/params.php
@@ -10,7 +10,7 @@ return [
     // 'user.passwordMinLength' => 8,
     'icon-framework' => 'fa',
 
-    'twilioSid' => 'AC3a735d7c9060e30aeb862ae99b408dd9',
-    'twiliotoken' => '41920774c53c332b9a9eafcbbb162dbf',
-    'twilioNumber' => '+85262027744',
+    // 'twilioSid' => 'AC3a735d7c9060e30aeb862ae99b408dd9',
+    // 'twiliotoken' => '41920774c53c332b9a9eafcbbb162dbf',
+    // 'twilioNumber' => '+85262027744',
 ];
